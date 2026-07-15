683b7383adbee064df9d055eb4d9eba1e05f994c
El-Vaibhav <135622906+El-Vaibhav@users.noreply.github.com>
2026-07-15 18:00:13 +0530

    commit fix

--- MAD_HOT_MAIN/context/AuthContext.tsx
+++ MAD_HOT_MAIN/context/AuthContext.tsx
@@ -2,7 +2,7 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 const API_BASE =
-  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
 const AuthContext = createContext<any>(null);
 
 export const AuthProvider = ({ children }: any) => {
